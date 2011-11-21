1105bba0ac232081e8c1b2d36efc1f0f0f18550a
Ashok Gelal <ashokgelal@gmail.com>
2011-11-20 22:03:29 -0700

    Add few utility methods

--- src/utils.go
+++ src/utils.go
@@ -1,5 +1,14 @@
 package config
 
-func ToString(name interface{}) string{
-    return name.(string)
+import "strings"
+
+func ToString(name interface{}) string {
+	return name.(string)
+}
+
+func TrimSpacesFromArray(val []string) []string {
+    for i, v := range val {
+        val[i] = strings.TrimSpace(v)
+    }
+    return val
 }
